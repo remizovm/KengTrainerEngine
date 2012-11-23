c6fb04e47dbed28ee673af408cabdd25484bc648
Mikhail Remizov <00keng@gmail.com>
2012-11-24 00:59:32 +0400

    Updated Loader comments

--- keng-loader/keng-loader.cpp
+++ keng-loader/keng-loader.cpp
@@ -49,7 +49,18 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_CREATE:
 		{
-			/*DWORD pID = GetTargetThreadIDFromProcName("d3d9_lesson0_keng.exe");
+			
+			break;
+		}
+	case WM_DESTROY:
+		{
+			PostQuitMessage(0);
+			break;
+		}
+	case WM_LBUTTONDOWN:
+		{
+			SendMessage(hWnd, 0xA1, 0x2, 0);
+			DWORD pID = GetTargetThreadIDFromProcName("d3d9_lesson0_keng.exe");
 			if(pID == 0)
 			{
 				MessageBox(0, "ERROR: Unable to find pID!", "Ocelot Loader @ keng", MB_OK);
@@ -62,17 +73,7 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 				GetFullPathName("keng-hook.dll", MAX_PATH, buf, 0);
 				Inject(pID, buf);
 				return 0;
-			}*/
-			break;
-		}
-	case WM_DESTROY:
-		{
-			PostQuitMessage(0);
-			break;
-		}
-	case WM_LBUTTONDOWN:
-		{
-			SendMessage(hWnd, 0xA1, 0x2, 0);
+			}
 			break;
 		}
 	case WM_RBUTTONUP:
@@ -82,18 +83,19 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		}
 	case WM_PAINT:
 		{
-			
 			hdc = BeginPaint(hWnd, &ps);
-			HPEN penWhite = CreatePen(BS_SOLID, 3, RGB(255, 255, 255));
-			HPEN penBlack = CreatePen(PS_SOLID, 3, RGB(0, 0, 0));
+			HPEN penWhite = CreatePen(PS_INSIDEFRAME, 3, RGB(255, 255, 255));
+			HPEN penBlack = CreatePen(PS_INSIDEFRAME, 3, RGB(0, 0, 0));
 			//HBRUSH hb = CreateSolidBrush(
 			SelectObject(hdc, penWhite);
 			//Rectangle(hdc, 4, 4, 236, 156);
 			//SelectObject(hdc, penBlack);
 			//Rectangle(hdc, 8, 8, 232, 152);
             MoveToEx(hdc, 4, 4, 0);
-			LineTo(hdc, 4, 156);
-			LineTo(hdc, 232, 156);
+			LineTo(hdc, 4, 155);
+			LineTo(hdc, 235, 155);
+			LineTo(hdc, 235, 4);
+			LineTo(hdc, 4, 4);
             EndPaint(hWnd, &ps);
 			break;
 		}
@@ -112,7 +114,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	wc.hInstance = hInstance;
 	wc.hCursor = LoadCursor(0, IDC_ARROW);
 	wc.lpszClassName = "WindowClass";
-	wc.hbrBackground = CreateSolidBrush(RGB(0, 0, 0));//(HBRUSH)COLOR_WINDOW;
+	wc.hbrBackground = CreateSolidBrush(RGB(0, 0, 0));
 	wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
 	RegisterClassEx(&wc);
@@ -129,39 +131,48 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	return msg.wParam;
 }
 
-DWORD GetTargetThreadIDFromProcName(const char * ProcName) 
-{ 
-   PROCESSENTRY32 pe; 
-   HANDLE thSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0); 
-   if(thSnapShot == INVALID_HANDLE_VALUE) 
-   { 
-      MessageBox(0, "ERROR: Unable to create toolhelp snapshot!", "Ocelot Loader @ keng", MB_OK); 
-      return 0; 
-   }   
-   pe.dwSize = sizeof(PROCESSENTRY32);
-   int retval = Process32First(thSnapShot, &pe); 
-   while(retval) 
-   { 
-      if(StrStrI(pe.szExeFile, ProcName)) return pe.th32ProcessID; 
-      retval = Process32Next(thSnapShot, &pe); 
-   } 
-   return 0; 
+//Game process ID searching function
+DWORD GetTargetThreadIDFromProcName(const char * procName) 
+{
+	PROCESSENTRY32 pe; //process snapshot
+	HANDLE thSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0); //getting the whole system processes snapshot
+	if(thSnapShot == INVALID_HANDLE_VALUE) //if something is wrong
+	{ //show error message
+		MessageBox(0, "ERROR: Unable to create toolhelp snapshot!", "Ocelot Loader @ keng", MB_OK); 
+		return 1; //and quit
+	}
+	pe.dwSize = sizeof(PROCESSENTRY32);
+	int retval = Process32First(thSnapShot, &pe); //get the 1-st process
+	while(retval) //while there are any processes left
+	{ 
+		if(StrStrI(pe.szExeFile, procName)) return pe.th32ProcessID; //if process name == our needed name, then return the pid and quit
+		retval = Process32Next(thSnapShot, &pe); //if not - get the next process
+	} 
+	return 0; //if nothing was found - return 0 and quit
 }
 
+//dll injection function
 int Inject(DWORD pID, const char * dllName) 
 { 
-   char buf[50] = {0}; 
-   if(!pID) return false;
-   HANDLE pHandle = OpenProcess(PROCESS_ALL_ACCESS, 0, pID); 
-   if(!pHandle) 
-   { 
-      MessageBox(0, "ERROR: Unable to open target process!", "Loader", MB_OK); 
-      return 1;
-   }
-   LPVOID loadLibAddr = (LPVOID)GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA"); 
-   LPVOID rString = (LPVOID)VirtualAllocEx(pHandle, 0, strlen(dllName), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE); 
-   WriteProcessMemory(pHandle, (LPVOID)rString, dllName, strlen(dllName), 0);
-   CreateRemoteThread(pHandle, 0, 0, (LPTHREAD_START_ROUTINE)loadLibAddr, (LPVOID)rString, 0, 0);
-   CloseHandle(pHandle);
-   return 0;
+	if(!pID) return 0; //if we've got no pID - quit
+	HANDLE pHandle = OpenProcess(PROCESS_ALL_ACCESS, 0, pID); //obtain the process handle
+	if(!pHandle) //in case of error - show error message and quit
+	{ 
+		MessageBox(0, "ERROR: Unable to open target process!", "Loader", MB_OK); 
+		return 1;
+	}
+	LPVOID loadLibAddr = (LPVOID)GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA"); //get the LoadLibraryA() address from kernel32.dll
+	//allocate a memory block for the full dll's path
+	LPVOID rString = (LPVOID)VirtualAllocEx(pHandle, 0, strlen(dllName), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE); 
+	//and write the path into target process memory
+	WriteProcessMemory(pHandle, (LPVOID)rString, dllName, strlen(dllName), 0);
+	//create a LoadLibrary(myDllPath) thread inside the target process
+	HANDLE hThread = CreateRemoteThread(pHandle, 0, 0, (LPTHREAD_START_ROUTINE)loadLibAddr, (LPVOID)rString, 0, 0);
+	if(hThread == 0)
+	{
+		MessageBox(0, "ERROR: Unable to create remote thread!", "Loader", MB_OK);
+		return 1;
+	}
+	CloseHandle(pHandle); //close the opened handle 
+	return 0; //everything is done, so quit
 } 
